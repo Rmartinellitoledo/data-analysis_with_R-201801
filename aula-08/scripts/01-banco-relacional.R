a7ca865c5d47a9371bc5ac0399608970860a3893
Rmartinellitoledo <rmartinellitoledo@gmail.com>
2018-06-12 01:27:15 +0000

    Aula 11/06

--- aula-08/scripts/01-banco-relacional.R
+++ aula-08/scripts/01-banco-relacional.R
@@ -1,11 +1,11 @@
 # biblioteca que implementa um Banco de Dados Relacional Tradicional, embutido (não executa um processo em separado)
-# install.packages("RSQLite")
+install.packages("RSQLite")
 
 # Banco de Dados Relacional e Colunar de propósito analítico, embutido
-# install.packages("MonetDBLite")
+install.packages("MonetDBLite")
 
 # Biblioteca para objetos JSON
-# install.packages("jsonlite")
+install.packages("jsonlite")
 
 library(tidyverse)
 
@@ -17,7 +17,20 @@ ted_talks <- read_csv("aula-08/data/ted_main.csv") %>%
 
 # Cria data.frame (tibble) ted_main com os atributos principais de uma talk
 ted_main <- ted_talks %>%
-  select(url, name, main_speaker, speaker_occupation, num_speaker, title, event, duration, film_date, published_date, comments, languages, views, description)
+  select(url, 
+         name, 
+         main_speaker, 
+         speaker_occupation, 
+         num_speaker, 
+         title, 
+         event, 
+         duration, 
+         film_date, 
+         published_date, 
+         comments, 
+         languages, 
+         views, 
+         description)
   
 # Cria data.frame (tibble) ted_ratings com cada categoria atribuida a uma ted_talk, a quantidade de vezes que a categoria foi votada e a proporção de votos de cada categoria
 ted_ratings <- ted_talks %>%
@@ -40,7 +53,11 @@ dbdir <- "aula-08/data/monetdb/ted"
 my_db <- MonetDBLite::src_monetdblite(dbdir)
 
 # Cria tabela temporária com ted_ratings
-tb_ted_ratings <- copy_to(my_db, df = ted_ratings, name = "ted_ratings_tmp", overwrite = TRUE, temporary = TRUE)
+tb_ted_ratings <- copy_to(my_db,
+                          df = ted_ratings,
+                          name = "ted_ratings_tmp",
+                          overwrite = TRUE,
+                          temporary = TRUE)
 
 # Cria tabela temporária com ted_main
 tb_ted_main <- copy_to(my_db, df = ted_main, name = "ted_main_tmp", overwrite = TRUE, temporary = TRUE)
@@ -62,14 +79,28 @@ tb_ted_main %>%
 show_query( tb_ted_main )
 
 # Grava ted_main com novas colunas
-tb_ted_main    <- copy_to( my_db, tb_ted_main, name = "ted_main", overwrite = TRUE, temporary = FALSE )
-tb_ted_ratings <- copy_to( my_db, tb_ted_ratings, name = "ted_ratings", overwrite = TRUE, temporary = FALSE )
+tb_ted_main    <- copy_to( my_db, 
+                           tb_ted_main, 
+                           name = "ted_main_AULA", 
+                           overwrite = TRUE, 
+                           temporary = FALSE )
+tb_ted_ratings <- copy_to( my_db, 
+                           tb_ted_ratings, 
+                           name = "ted_ratings_AULA", 
+                           overwrite = TRUE, 
+                           temporary = FALSE )
 
 # Encerra conexão
 MonetDBLite::monetdblite_shutdown()
 
 # Remove data frames
-rm(my_db, tb_ted_main, tb_ted_ratings, ted_defining_category, ted_main, ted_ratings, ted_talks)
+rm(my_db, 
+   tb_ted_main, 
+   tb_ted_ratings, 
+   ted_defining_category, 
+   ted_main, 
+   ted_ratings, 
+   ted_talks)
 
 # Nova conexão
 dbdir <- "aula-08/data/monetdb/ted"
@@ -79,6 +110,7 @@ teds <- tbl( my_db_new_conn, "ted_main" )
 
 head(teds, 10)
 
-head(teds, 10) %>% show_query()
+head(teds, 10) %>% 
+  show_query()
 
 MonetDBLite::monetdblite_shutdown()
